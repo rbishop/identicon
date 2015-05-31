c1fa99626fce4a9625272aff84773cdf0da1bac3
Richard Bishop <richard@rubiquity.com>
2015-05-31 16:10:02 -0600

    Remove two-dimensional list representing grid, rename points to pixels

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,66 +1,62 @@
 defmodule Identicon do
   @moduledoc """
-    This library generates GitHub-like identicons.
+    This library generates GitHub-like identicons. Unlike
+    GitHub's identicons, the identicons this library generates
+    are not symmetrical.
+
     Given an input of a string you will receive a base64
     encoded png of 5x5 identicon for that string.
   """
-
-  defstruct color: nil, hex: nil, grid: nil, md5: nil, points: nil
+  defstruct color: nil, hex: nil, grid: nil, md5: nil, pixels: nil
 
   def render(input) do
     input 
     |> hash_input
     |> extract_color
     |> build_grid
-    |> calculate_squares
+    |> calculate_pixels
     |> draw_image
   end
 
   @spec hash_input(String.t) :: map(Identicon)
-  def hash_input(string) do
+  defp hash_input(string) do
     hex = :crypto.hash(:md5, string) |> :binary.bin_to_list
     md5 = Enum.flat_map(hex, &(:io_lib.format("~2.16.0b", [&1]))) |> Enum.join("")
     %Identicon{hex: hex, md5: md5}
   end
 
-  def extract_color(%Identicon{hex: [r, g, b | _]} = identicon) do
+  defp extract_color(%Identicon{hex: [r, g, b | _]} = identicon) do
     %Identicon{identicon | color: {r, g, b}}
   end
 
-  def build_grid(%Identicon{md5: md5} = identicon) do
+  defp build_grid(%Identicon{md5: md5} = identicon) do
     grid = String.slice(md5, 6..30)
     |> String.split("", trim: true)
     |> Enum.map(&(:erlang.binary_to_integer(&1, 16)))
-    |> Enum.chunk(5)
-    |> Enum.map(&Enum.with_index/1)
     |> Enum.with_index
+    |> Enum.filter(fn({code, _index}) -> rem(code, 2) == 0 end)
 
     %Identicon{identicon | grid: grid}
   end
 
-  def calculate_squares(%Identicon{grid: grid} = identicon) do
-    points = Enum.flat_map(grid, fn({cells, row}) ->
-      Enum.map(cells, fn({cell, column}) ->
-        case rem(cell, 3) do
-          0 ->
-            top_left = {row * 50, column * 50}
-            bottom_right = {row * 50 + 50, column * 50 + 50}
-            {top_left, bottom_right}
-          _ ->
-            {}
-        end
-      end)
-      |> Enum.reject(&(&1 == {}))
+  defp calculate_pixels(%Identicon{grid: grid} = identicon) do
+    pixels = Enum.map(grid, fn({_code, index}) ->
+      row = div(index, 5)
+      column = rem(index, 5)
+
+      top_left = {row * 50, column * 50}
+      bottom_right = {row * 50 + 50, column * 50 + 50}
+      {top_left, bottom_right}
     end)
 
-    %Identicon{identicon | points: points}
+    %Identicon{identicon | pixels: pixels}
   end
 
-  def draw_image(%Identicon{color: color, points: points}) do
+  defp draw_image(%Identicon{color: color, pixels: pixels}) do
     image = :egd.create(250, 250)
     fill = :egd.color(color)
 
-    Enum.each(points, fn({start, stop}) -> 
+    Enum.each(pixels, fn({start, stop}) -> 
       :egd.filledRectangle(image, start, stop, fill)
     end)
 
